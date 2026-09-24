0647f2ef6f310f374d0dc1495b3a573faa33ab99
grira_amine <130407248+Griraamine@users.noreply.github.com>
2026-09-24 15:14:28 +0200

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,14 @@
+from collections import defaultdict
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t) : return False
+        
+        
+        seen = defaultdict(int)
+
+
+        for i in range(len(s)):
+            seen[s[i]] += 1
+            seen[t[i]] -= 1
+
+        return all(value == 0 for value in seen.values())
